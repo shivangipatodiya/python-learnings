a7dce109cd96b047809ef0c6dc988128a7b7584f
shivangipatodiya <shivu2610@gmail.com>
2023-06-28 23:18:33 -0400

    Update NATO-alphabet project with error handling

--- NATO-alphabet/main.py
+++ NATO-alphabet/main.py
@@ -7,7 +7,17 @@ new_dict = {row.letter: row.code for (index, row) in data.iterrows()}
 
 #TODO 2. Create a list of the phonetic code words from a word that the user inputs.
 
-user_input = input("Write a word: ").upper()
 
-new_list = [new_dict[letter] for letter in user_input]
-print(new_list)
+def generate_phonetic():
+    user_input = input("Write a word: ").upper()
+
+    try:
+        new_list = [new_dict[letter] for letter in user_input]
+    except KeyError:
+        print("Sorry, only letters in alphabet please.")
+        generate_phonetic()
+    else:
+        print(new_list)
+
+
+generate_phonetic()
