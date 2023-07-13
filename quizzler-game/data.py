401289efb215bb2236175e88ac95f4f85551aaa2
shivangipatodiya <shivu2610@gmail.com>
2023-07-13 18:07:19 -0400

    make quizzler game functional

--- quizzler-game/data.py
+++ quizzler-game/data.py
@@ -7,4 +7,5 @@ parameters = {
 
 response = requests.get("https://opentdb.com/api.php", params=parameters)
 response.raise_for_status()
-question_data = response.json()["results"]
+data = response.json()
+question_data = data["results"]
